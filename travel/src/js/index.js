0fde500e16902e8bf1eaddd4bec287541dde88d3
Vitaliy Zayakin <vitaliy.zayakin@gmail.com>
2022-07-03 16:31:02 +0300

    feat: add burger menu prototype

--- travel/src/js/index.js
+++ travel/src/js/index.js
@@ -1,9 +1,20 @@
-const articles = document.querySelectorAll(".article__text");
-document.addEventListener("DOMContentLoaded", (e) => {
-  if (window.innerWidth <= 390) {
-    articles.forEach((article) => {
-      article.textContent =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.Lorem ipsum dolor sit a... ";
-    });
-  }
+const burgerOpen = document.querySelector(".header__burger");
+const burger = document.querySelector(".burger");
+const burgerClose = document.querySelector(".burger__btn_icon");
+const burgerWrap = document.querySelector(".burger__wrap");
+
+burgerOpen.addEventListener("click", (e) => {
+  burgerWrap.style.display = "block";
+  burgerWrap.style.animation = "bgc-black 0.5s linear 1 forwards";
+  burger.style.animation = "move-left 0.5s linear 1 forwards";
+
+  // добавить листенер на ссылки и тело враппера
+});
+
+burgerClose.addEventListener("click", (e) => {
+  burger.style.animation = "move-right 0.5s linear 1 forwards";
+  burgerWrap.style.animation = "bgc-none 0.5s linear 1 forwards";
+  setTimeout(() => {
+    burgerWrap.style.display = "none";
+  }, 500);
 });
