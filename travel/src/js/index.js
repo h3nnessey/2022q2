466f6f99af2fc17867f07250650470ca73054756
Vitaliy Zayakin <vitaliy.zayakin@gmail.com>
2022-07-04 00:54:39 +0300

    refactor: refactor

--- travel/src/js/index.js
+++ travel/src/js/index.js
@@ -2,50 +2,37 @@ const burgerOpen = document.querySelector(".header__burger");
 const burger = document.querySelector(".burger");
 const burgerClose = document.querySelector(".burger__btn_icon");
 const burgerWrap = document.querySelector(".burger__wrap");
+const links = document.querySelectorAll(".burger__link");
+
+function show() {
+    burgerWrap.style.display = "block";
+    burgerWrap.style.animation = "bgc-black 0.3s linear 1 forwards";
+    burger.style.animation = "move-left 0.3s linear 1 forwards";
+}
+
+function hide() {
+    burger.style.animation = "move-right 0.3s linear 1 forwards";
+    burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
+    setTimeout(() => {
+        burgerWrap.style.display = "none";
+    }, 300);
+}
 
 burgerOpen.addEventListener("click", (e) => {
-  burgerWrap.style.display = "block";
-  burgerWrap.style.animation = "bgc-black 0.3s linear 1 forwards";
-  burger.style.animation = "move-left 0.3s linear 1 forwards";
-
-  const links = document.querySelectorAll(".burger__link");
-  links.forEach((link) => {
-    link.addEventListener("click", (e) => {
-
-      burger.style.animation = "move-right 0.3s linear 1 forwards";
-      burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
-      
-      setTimeout(() => {
-        burgerWrap.style.display = "none";
-      }, 300);
-      
+    show();
+    links.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            hide();
+        });
     });
-  });
 
-  burgerWrap.addEventListener("click", (e) => {
-
-    if (e.target.className === "burger__wrap") {
-
-      burger.style.animation = "move-right 0.3s linear 1 forwards";
-      burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
-
-      setTimeout(() => {
-        burgerWrap.style.display = "none";
-      }, 300);
-    }
-  });
-
+    burgerWrap.addEventListener("click", (e) => {
+        if (e.target.className === "burger__wrap") {
+            hide()
+        }
+    });
 });
 
 burgerClose.addEventListener("click", (e) => {
-
-  burger.style.animation = "move-right 0.3s linear 1 forwards";
-  burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
-
-  setTimeout(() => {
-    burgerWrap.style.display = "none";
-  }, 300);
-
+    hide()
 });
-
-// вынести в отдельные функции DRY
