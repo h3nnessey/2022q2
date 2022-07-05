78a12db6498e215bcc8c6ef77cc0d868b425801e
Vitaliy Zayakin <vitaliy.zayakin@gmail.com>
2022-07-05 13:13:23 +0300

    refactor: refactor

--- travel/src/js/index.js
+++ travel/src/js/index.js
@@ -5,34 +5,33 @@ const burgerWrap = document.querySelector(".burger__wrap");
 const links = document.querySelectorAll(".burger__link");
 
 function show() {
-    burgerWrap.style.display = "block";
-    burgerWrap.style.animation = "bgc-black 0.3s linear 1 forwards";
-    burger.style.animation = "move-left 0.3s linear 1 forwards";
+  burgerWrap.style.display = "block";
+  burgerWrap.style.animation = "bgc-black 0.3s linear 1 forwards";
+  burger.style.animation = "move-left 0.3s linear 1 forwards";
 }
 
 function hide() {
-    burger.style.animation = "move-right 0.3s linear 1 forwards";
-    burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
-    setTimeout(() => {
-        burgerWrap.style.display = "none";
-    }, 300);
+  burger.style.animation = "move-right 0.3s linear 1 forwards";
+  burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
+  const hideBurgerWrap = () => (burgerWrap.style.display = "none");
+  setTimeout(hideBurgerWrap, 300);
 }
 
 burgerOpen.addEventListener("click", (e) => {
-    show();
-    links.forEach((link) => {
-        link.addEventListener("click", (e) => {
-            hide();
-        });
+  show();
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      hide();
     });
+  });
 
-    burgerWrap.addEventListener("click", (e) => {
-        if (e.target.className === "burger__wrap") {
-            hide()
-        }
-    });
+  burgerWrap.addEventListener("click", (e) => {
+    if (e.target.className === "burger__wrap") {
+      hide();
+    }
+  });
 });
 
 burgerClose.addEventListener("click", (e) => {
-    hide()
+  hide();
 });
