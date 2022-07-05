185d78e5003051d6a454366cea25b44136c83c54
Vitaliy Zayakin <vitaliy.zayakin@gmail.com>
2022-07-05 15:37:28 +0300

    feat: add gh & rss links, add disabled scroll while burger is active

--- travel/src/js/index.js
+++ travel/src/js/index.js
@@ -5,12 +5,14 @@ const burgerWrap = document.querySelector(".burger__wrap");
 const links = document.querySelectorAll(".burger__link");
 
 function show() {
+  document.body.style.overflow = 'hidden';
   burgerWrap.style.display = "block";
   burgerWrap.style.animation = "bgc-black 0.3s linear 1 forwards";
   burger.style.animation = "move-left 0.3s linear 1 forwards";
 }
 
 function hide() {
+  document.body.style.overflow = 'initial';
   burger.style.animation = "move-right 0.3s linear 1 forwards";
   burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
   const hideBurgerWrap = () => (burgerWrap.style.display = "none");
