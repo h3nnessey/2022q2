70dcc9ce8969861139c1f4e4b5f1bbe1c71a6c46
Vitaliy Zayakin <vitaliy.zayakin@gmail.com>
2022-07-07 21:46:56 +0300

    feat: add mobile slider proto

--- travel/src/js/index.js
+++ travel/src/js/index.js
@@ -6,6 +6,8 @@ const links = document.querySelectorAll(".burger__link");
 const items = document.querySelectorAll(".photo__wrapper");
 const slider = document.querySelector(".slider__inner");
 const sliderBtns = document.querySelectorAll(".slider__button");
+let arrLeft = document.querySelector(".slider__arrow-left");
+let arrRight = document.querySelector(".slider__arrow-right");
 
 function show() {
   document.body.style.overflow = "hidden";
@@ -44,50 +46,186 @@ burgerClose.addEventListener("click", () => {
 function setActiveBtn(id) {
   sliderBtns.forEach((btn, idx) => {
     if (idx !== id) {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
     } else {
-      btn.classList.add('active')
+      btn.classList.add("active");
     }
   });
 }
 
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.innerWidth > 390) {
+    slider.style.transform = 'translateX(0px)';
+    setActiveBtn(1);
+
+    items[1].addEventListener("click", desk1);
+    items[2].addEventListener("click", desk2);
+    items[3].addEventListener("click", desk3);
+
+    sliderBtns[0].addEventListener("click", desk1);
+    sliderBtns[1].addEventListener("click", desk2);
+    sliderBtns[2].addEventListener("click", desk3);
+  }
+
+  if (window.innerWidth <= 390) {
+    slider.style.transform = "translateX(0px)";
+    setActiveBtn(0);
+    arrLeft.style.opacity = '0.5';
+    arrRight.style.opacity = '1';
+
+    items[1].removeEventListener("click", desk1);
+    items[2].removeEventListener("click", desk2);
+    items[3].removeEventListener("click", desk3);
+    // кнопки можно сделать неактив
+    sliderBtns[0].removeEventListener("click", desk1);
+    sliderBtns[1].removeEventListener("click", desk2);
+    sliderBtns[2].removeEventListener("click", desk3);
+
+    let curr = 0;
+
+    sliderBtns[0].addEventListener("click", () => {
+      slider.style.transform = "translateX(0px)";
+    });
+
+    sliderBtns[1].addEventListener("click", () => {
+      slider.style.transform = "translateX(-420px)";
+    });
+
+    sliderBtns[2].addEventListener("click", () => {
+      slider.style.transform = "translateX(-840px)";
+    });
+
+    arrRight.addEventListener("click", (e) => {
+      if (curr === 0) {
+        slider.style.transform = "translateX(-420px)";
+        arrLeft.style.opacity = "1";
+        setTimeout(() => {
+          curr++;
+        }, 100);
+      }
+      if (curr === 1) {
+        slider.style.transform = "translateX(-840px)";
+        setTimeout(() => {
+          curr++;
+        }, 100);
+        arrRight.style.opacity = "0.5";
+      }
+    });
+
+    arrLeft.addEventListener("click", (e) => {
+      if (curr === 1) {
+        slider.style.transform = "translateX(0px)";
+        arrLeft.style.opacity = "0.5";
+        setTimeout(() => {
+          curr--;
+        }, 100);
+      }
+      if (curr === 2) {
+        arrRight.style.opacity = "1";
+        slider.style.transform = "translateX(-420px)";
+        setTimeout(() => {
+          curr--;
+        }, 100);
+      }
+    });
+  }
+});
 
 
-items[1].addEventListener("click", () => {
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 390) {
+    slider.style.transform = 'translateX(0px)';
+    setActiveBtn(1);
+
+    items[1].addEventListener("click", desk1);
+    items[2].addEventListener("click", desk2);
+    items[3].addEventListener("click", desk3);
+
+    sliderBtns[0].addEventListener("click", desk1);
+    sliderBtns[1].addEventListener("click", desk2);
+    sliderBtns[2].addEventListener("click", desk3);
+  }
+
+  if (window.innerWidth <= 390) {
+    slider.style.transform = "translateX(0px)";
+    setActiveBtn(0);
+
+    items[1].removeEventListener("click", desk1);
+    items[2].removeEventListener("click", desk2);
+    items[3].removeEventListener("click", desk3);
+    // кнопки можно сделать неактив
+    sliderBtns[0].removeEventListener("click", desk1);
+    sliderBtns[1].removeEventListener("click", desk2);
+    sliderBtns[2].removeEventListener("click", desk3);
+
+    let curr = 0;
+
+    sliderBtns[0].addEventListener("click", () => {
+      slider.style.transform = "translateX(0px)";
+    });
+
+    sliderBtns[1].addEventListener("click", () => {
+      slider.style.transform = "translateX(-420px)";
+    });
+
+    sliderBtns[2].addEventListener("click", () => {
+      slider.style.transform = "translateX(-840px)";
+    });
+
+    arrRight.addEventListener("click", (e) => {
+      if (curr === 0) {
+        slider.style.transform = "translateX(-420px)";
+        arrLeft.style.opacity = "1";
+        setTimeout(() => {
+          curr++;
+        }, 100);
+      }
+      if (curr === 1) {
+        slider.style.transform = "translateX(-840px)";
+        setTimeout(() => {
+          curr++;
+        }, 100);
+        arrRight.style.opacity = "0.5";
+      }
+    });
+
+    arrLeft.addEventListener("click", (e) => {
+      if (curr === 1) {
+        slider.style.transform = "translateX(0px)";
+        arrLeft.style.opacity = "0.5";
+        setTimeout(() => {
+          curr--;
+        }, 100);
+      }
+      if (curr === 2) {
+        arrRight.style.opacity = "1";
+        slider.style.transform = "translateX(-420px)";
+        setTimeout(() => {
+          curr--;
+        }, 100);
+      }
+    });
+  }
+});
+
+function desk1() {
   slider.style.transform = "translateX(860px)";
   setActiveBtn(0);
-});
-
-items[2].addEventListener("click", () => {
+}
+function desk2() {
   slider.style.transform = "translateX(0px)";
   setActiveBtn(1);
-});
-
-items[3].addEventListener("click", () => {
+}
+function desk3() {
   slider.style.transform = "translateX(-860px)";
   setActiveBtn(2);
-});
-
-// на мобиле хз надо ли
-sliderBtns[0].addEventListener("click", () => {
-  slider.style.transform = "translateX(860px)";
-  setActiveBtn(0);
-});
-
-sliderBtns[1].addEventListener("click", () => {
-  slider.style.transform = "translateX(0px)";
-  setActiveBtn(1);
-});
-
-sliderBtns[2].addEventListener("click", () => {
-  slider.style.transform = "translateX(-860px)";
-  setActiveBtn(2);
-});
+}
 
 // rename variables
-// max-width 390
-// в тревел 2 просто ебануть через css актив кнопки на слайдере (через медиа)
-// убрать к хуям гх и рс скул
 
-// вынести десктоп в отдельную функцию и закинуть ее в ресайз и домконтентлоудед
-// потом делать под мобилу
+// задать стили на футер из тревел2
+
+// надо сильно увеличить область клика на стрелках - иначе баги
+
+
+// закончить логику со слайдер кнопками
