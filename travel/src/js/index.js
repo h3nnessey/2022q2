d1b3b38407a813be537f06fc4d3f261b2166a710
Vitaliy Zayakin <vitaliy.zayakin@gmail.com>
2022-07-08 14:58:41 +0300

    feat: atleast done both of sliders

--- travel/src/js/index.js
+++ travel/src/js/index.js
@@ -9,21 +9,6 @@ const sliderBtns = document.querySelectorAll(".slider__button");
 let arrLeft = document.querySelector(".slider__arrow-left");
 let arrRight = document.querySelector(".slider__arrow-right");
 
-function show() {
-  document.body.style.overflow = "hidden";
-  burgerWrap.style.display = "block";
-  burgerWrap.style.animation = "bgc-black 0.3s linear 1 forwards";
-  burger.style.animation = "move-left 0.3s linear 1 forwards";
-}
-
-function hide() {
-  document.body.style.overflow = "initial";
-  burger.style.animation = "move-right 0.3s linear 1 forwards";
-  burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
-  const hideBurgerWrap = () => (burgerWrap.style.display = "none");
-  setTimeout(hideBurgerWrap, 300);
-}
-
 burgerOpen.addEventListener("click", () => {
   show();
   links.forEach((link) => {
@@ -43,6 +28,100 @@ burgerClose.addEventListener("click", () => {
   hide();
 });
 
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.innerWidth > 390) {
+    initDesktopSlider();
+  }
+  if (window.innerWidth <= 390) {
+    initMobileSlider();
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 390) {
+    initDesktopSlider();
+  }
+  if (window.innerWidth <= 390) {
+    initMobileSlider();
+  }
+});
+
+function show() {
+  document.body.style.overflow = "hidden";
+  burgerWrap.style.display = "block";
+  burgerWrap.style.animation = "bgc-black 0.3s linear 1 forwards";
+  burger.style.animation = "move-left 0.3s linear 1 forwards";
+}
+
+function hide() {
+  document.body.style.overflow = "initial";
+  burger.style.animation = "move-right 0.3s linear 1 forwards";
+  burgerWrap.style.animation = "bgc-none 0.3s linear 1 forwards";
+  const hideBurgerWrap = () => (burgerWrap.style.display = "none");
+  setTimeout(hideBurgerWrap, 300);
+}
+
+function initDesktopSlider() {
+  desk2();
+  items[1].addEventListener("click", desk1);
+  items[2].addEventListener("click", desk2);
+  items[3].addEventListener("click", desk3);
+  sliderBtns[0].addEventListener("click", desk1);
+  sliderBtns[1].addEventListener("click", desk2);
+  sliderBtns[2].addEventListener("click", desk3);
+}
+
+function initMobileSlider() {
+  mob1();
+  setOpacity(arrLeft, 0.5);
+  setOpacity(arrRight, 1);
+  let curr = 0;
+
+  items[1].removeEventListener("click", desk1);
+  items[2].removeEventListener("click", desk2);
+  items[3].removeEventListener("click", desk3);
+
+  sliderBtns[0].removeEventListener("click", desk1);
+  sliderBtns[1].removeEventListener("click", desk2);
+  sliderBtns[2].removeEventListener("click", desk3);
+
+  arrRight.addEventListener("click", moveRight);
+  arrLeft.addEventListener("click", moveLeft);
+
+  function moveRight() {
+    if (curr === 0) {
+      mob2();
+      setOpacity(arrLeft, 1);
+      setTimeout(() => {
+        curr++;
+      }, 100);
+    }
+    if (curr === 1) {
+      mob3();
+      setOpacity(arrRight, 0.5);
+      setTimeout(() => {
+        curr++;
+      }, 100);
+    }
+  }
+  function moveLeft() {
+    if (curr === 1) {
+      mob1();
+      setOpacity(arrLeft, 0.5);
+      setTimeout(() => {
+        curr--;
+      }, 100);
+    }
+    if (curr === 2) {
+      setOpacity(arrRight, 1);
+      mob2();
+      setTimeout(() => {
+        curr--;
+      }, 100);
+    }
+  }
+}
+
 function setActiveBtn(id) {
   sliderBtns.forEach((btn, idx) => {
     if (idx !== id) {
@@ -52,161 +131,6 @@ function setActiveBtn(id) {
     }
   });
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  if (window.innerWidth > 390) {
-    slider.style.transform = 'translateX(0px)';
-    setActiveBtn(1);
-
-    items[1].addEventListener("click", desk1);
-    items[2].addEventListener("click", desk2);
-    items[3].addEventListener("click", desk3);
-
-    sliderBtns[0].addEventListener("click", desk1);
-    sliderBtns[1].addEventListener("click", desk2);
-    sliderBtns[2].addEventListener("click", desk3);
-  }
-
-  if (window.innerWidth <= 390) {
-    slider.style.transform = "translateX(0px)";
-    setActiveBtn(0);
-    arrLeft.style.opacity = '0.5';
-    arrRight.style.opacity = '1';
-
-    items[1].removeEventListener("click", desk1);
-    items[2].removeEventListener("click", desk2);
-    items[3].removeEventListener("click", desk3);
-    // кнопки можно сделать неактив
-    sliderBtns[0].removeEventListener("click", desk1);
-    sliderBtns[1].removeEventListener("click", desk2);
-    sliderBtns[2].removeEventListener("click", desk3);
-
-    let curr = 0;
-
-    sliderBtns[0].addEventListener("click", () => {
-      slider.style.transform = "translateX(0px)";
-    });
-
-    sliderBtns[1].addEventListener("click", () => {
-      slider.style.transform = "translateX(-420px)";
-    });
-
-    sliderBtns[2].addEventListener("click", () => {
-      slider.style.transform = "translateX(-840px)";
-    });
-
-    arrRight.addEventListener("click", (e) => {
-      if (curr === 0) {
-        slider.style.transform = "translateX(-420px)";
-        arrLeft.style.opacity = "1";
-        setTimeout(() => {
-          curr++;
-        }, 100);
-      }
-      if (curr === 1) {
-        slider.style.transform = "translateX(-840px)";
-        setTimeout(() => {
-          curr++;
-        }, 100);
-        arrRight.style.opacity = "0.5";
-      }
-    });
-
-    arrLeft.addEventListener("click", (e) => {
-      if (curr === 1) {
-        slider.style.transform = "translateX(0px)";
-        arrLeft.style.opacity = "0.5";
-        setTimeout(() => {
-          curr--;
-        }, 100);
-      }
-      if (curr === 2) {
-        arrRight.style.opacity = "1";
-        slider.style.transform = "translateX(-420px)";
-        setTimeout(() => {
-          curr--;
-        }, 100);
-      }
-    });
-  }
-});
-
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 390) {
-    slider.style.transform = 'translateX(0px)';
-    setActiveBtn(1);
-
-    items[1].addEventListener("click", desk1);
-    items[2].addEventListener("click", desk2);
-    items[3].addEventListener("click", desk3);
-
-    sliderBtns[0].addEventListener("click", desk1);
-    sliderBtns[1].addEventListener("click", desk2);
-    sliderBtns[2].addEventListener("click", desk3);
-  }
-
-  if (window.innerWidth <= 390) {
-    slider.style.transform = "translateX(0px)";
-    setActiveBtn(0);
-
-    items[1].removeEventListener("click", desk1);
-    items[2].removeEventListener("click", desk2);
-    items[3].removeEventListener("click", desk3);
-    // кнопки можно сделать неактив
-    sliderBtns[0].removeEventListener("click", desk1);
-    sliderBtns[1].removeEventListener("click", desk2);
-    sliderBtns[2].removeEventListener("click", desk3);
-
-    let curr = 0;
-
-    sliderBtns[0].addEventListener("click", () => {
-      slider.style.transform = "translateX(0px)";
-    });
-
-    sliderBtns[1].addEventListener("click", () => {
-      slider.style.transform = "translateX(-420px)";
-    });
-
-    sliderBtns[2].addEventListener("click", () => {
-      slider.style.transform = "translateX(-840px)";
-    });
-
-    arrRight.addEventListener("click", (e) => {
-      if (curr === 0) {
-        slider.style.transform = "translateX(-420px)";
-        arrLeft.style.opacity = "1";
-        setTimeout(() => {
-          curr++;
-        }, 100);
-      }
-      if (curr === 1) {
-        slider.style.transform = "translateX(-840px)";
-        setTimeout(() => {
-          curr++;
-        }, 100);
-        arrRight.style.opacity = "0.5";
-      }
-    });
-
-    arrLeft.addEventListener("click", (e) => {
-      if (curr === 1) {
-        slider.style.transform = "translateX(0px)";
-        arrLeft.style.opacity = "0.5";
-        setTimeout(() => {
-          curr--;
-        }, 100);
-      }
-      if (curr === 2) {
-        arrRight.style.opacity = "1";
-        slider.style.transform = "translateX(-420px)";
-        setTimeout(() => {
-          curr--;
-        }, 100);
-      }
-    });
-  }
-});
 
 function desk1() {
   slider.style.transform = "translateX(860px)";
@@ -221,11 +145,29 @@ function desk3() {
   setActiveBtn(2);
 }
 
-// rename variables
+function mob1() {
+  slider.style.transform = "translateX(0px)";
+  setActiveBtn(0);
+}
+
+function mob2() {
+  slider.style.transform = "translateX(-420px)";
+  setActiveBtn(1);
+}
+
+function mob3() {
+  slider.style.transform = "translateX(-840px)";
+  setActiveBtn(2);
+}
+
+function setOpacity(el, n) {
+  el.style.opacity = `${n}`;
+}
+
+// rename variables - items и прочий кал + функции
 
 // задать стили на футер из тревел2
 
 // надо сильно увеличить область клика на стрелках - иначе баги
 
-
-// закончить логику со слайдер кнопками
+// повесить курсор поинтеры на десктопе
